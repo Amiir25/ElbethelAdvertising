28ba81bd55e97fa142748c63701451d8ab9fbeef
Amiir25 <amirsadik636@gmail.com>
2025-10-27 18:45:58 +0300

    Add Footer component in Footer.tsx

---
+++ elbethel/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='mt-20'>
+      <p className='text-center text-sm bg-amber-300 p-2 tracking-wider'>
+        &copy; 2025 Elbethel Advertising & Printing. All rights reserved.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
