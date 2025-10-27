43760c4fdc3152ac1c9b68eb2bc4fb5ae5414151
Amiir25 <amirsadik636@gmail.com>
2025-10-27 18:48:10 +0300

    Add Services.tsx component in Services.tsx

---
+++ elbethel/components/Services.tsx
@@ -0,0 +1,36 @@
+import { services } from '@/constants/services'
+import Image from 'next/image'
+import React from 'react'
+import Title from './Title'
+
+const Services = () => {
+  return (
+    <section className='mt-20'>
+      
+      <Title 
+        normalTitle='Our'
+        underlineTitle='Services'
+        subtitle='From custom prints to complete branding solutions, we deliver quality that speaks for your brand.' />
+
+      {/*  */}
+      <div className='md:flex flex-wrap items-center justify-center gap-10'>
+        {
+          services.map((service, i) => (
+            <div key={i} className='mt-10 w-120'>
+              <div className='w-full h-60'>
+                <Image src={service.image} alt={service.title} className='' />
+              </div>
+              <div>
+                <h1 className='text-xl font-bold'>{ service.title }</h1>
+                <p className='text-sm'>{ service.description }</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
+    </section>
+  )
+}
+
+export default Services
