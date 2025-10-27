b868c91c98bc9ffb1529f99b111a67a17d729178
Amiir25 <amirsadik636@gmail.com>
2025-10-27 18:48:59 +0300

    Add Testimonials.tsx component in Testimonials.tsx

---
+++ elbethel/components/Testimonials.tsx
@@ -0,0 +1,32 @@
+import { testimonials } from "@/constants/testimonials";
+import Title from "./Title";
+
+const Testimonials = () => {
+
+  return (
+    <section className="mt-40 overflow-hidden">
+      
+      <Title
+        normalTitle="What Our Clients "
+        underlineTitle="Say?"
+        subtitle="Don't just take our word for it — hear from our satisfied customers" />
+
+      <div className='mt-10 flex flex-col md:flex-row flex-wrap justify-center gap-x-20 gap-y-10'>
+        {
+          testimonials.map((testimony, i) => (
+            <div key={i} 
+            className="w-80 md:w-100 flex flex-col px-4 py-10 border-t-5 border-b-5 border-amber-300 rounded-xl" >
+              <p className="font-serif tracking-wide">{ testimony.text }</p>
+              <div className="mt-8 text-sm pt-4 border-t border-gray-200">
+                <h2 className="font-semibold">{ testimony.name }</h2>
+                <h3 className="text-gray-600">{ testimony.profession }</h3>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </section>
+  );
+}
+
+export default Testimonials;
