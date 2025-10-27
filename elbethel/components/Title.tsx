05ca15288a3f04884a066d87f26b196a4d1dd88c
Amiir25 <amirsadik636@gmail.com>
2025-10-27 18:49:21 +0300

    Add Title.tsx component in Title.tsx

---
+++ elbethel/components/Title.tsx
@@ -0,0 +1,21 @@
+type title = {
+    normalTitle: string;
+    underlineTitle: string;
+    subtitle?: string
+}
+
+const Title = ({ normalTitle, underlineTitle, subtitle }: title) => {
+  return (
+    <section>
+        <h1 className='text-2xl md:text-3xl lg:text-4xl text-center'>
+          { normalTitle } <span className='underline underline-offset-6 decoration-5 decoration-amber-300'>{ underlineTitle }</span>
+        </h1>
+        <p className='text-center text-sm md:text-base font-light mt-4'>
+          { subtitle }
+        </p>
+
+    </section>
+  )
+}
+
+export default Title
