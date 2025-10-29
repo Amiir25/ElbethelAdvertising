59082b2e4f4c23b25e49acbcded7f0028ad32e52
Amiir25 <amirsadik636@gmail.com>
2025-10-29 14:56:48 +0300

    Update the story and add Read More/Less feature in About.tsx

--- elbethel/components/About.tsx
+++ elbethel/components/About.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+"use client"
+
+import { useState } from 'react'
 import Title from './Title'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons/faArrowUp';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons/faArrowDown';
 
 const About = () => {
+  
+  const [expand, setExpand] = useState(false);
+
   return (
     <section className='mt-20'>
 
@@ -9,15 +17,39 @@ const About = () => {
         normalTitle='Our'
         underlineTitle='Story' />
 
-      <p className='mt-10 text-sm md:text-lg tracking-wide md:w-180 mx-auto text-justify'>
-        Elbethel Advertising is a leading advertising and printing company based in Addis Ababa, Ethiopia, specializing in neon lights, light boxes, and custom signage
-        solutions that bring brands to life. We also supply a wide range of advertising and printing materials and equipment, including acrylic bending machines, power
-        supplies, neon tubes, CNC routers, and laser cutting machines.
-        <br /><br />
-        With a strong focus on creativity, quality, and innovation, Elbethel helps businesses of all sizes stand out with eye-catching illuminated signs and professional
-        printing services. Our mission is to provide reliable products, expert craftsmanship, and modern solutions that empower our clients to showcase their brand in the
-        brightest light.
-      </p>
+      <div className={` relative overflow-hidden ${ expand ? 'max-h-300' : 'max-h-80' }
+      transition-all duration-500 `}>
+        <p className='mt-10 text-sm md:text-lg tracking-wide md:w-180 mx-auto text-justify'>
+          Elbethel Advertising is a creative advertising and printing company based in Addis Ababa, Ethiopia, specializing in LED signage, light boxes, and high-quality
+          digital printing solutions. We help businesses stand out through eye-catching indoor and outdoor advertising materials designed to make every brand shine.
+          <br /><br />
+          Our core expertise includes LED lighting letters, light boxes, indoor and outdoor signage, wall branding, and large-format digital printing. In addition,
+          we produce custom banners, interior signs, vehicle graphics, and personalized promotional items such as T-shirts, pens, medals, and key holders.
+          <br /><br />
+          Beyond design and production, Elbethel also supplies advertising and printing materials—including acrylic bending machines, power supplies, neon tubes, CNC routers,
+          and laser cutting machines—helping other businesses create and maintain their own signage projects.
+          <br /><br />
+          Driven by creativity, quality, and precision, we’re committed to delivering innovative visual solutions that help brands light up their message and leave
+          a lasting impression.
+        </p>
+
+        {/* Shadow */}
+        {
+          !expand &&
+          <div className='absolute bottom-0 md:bottom-6 left-0 w-full h-16
+          bg-gradient-to-t from-white to-transparent'></div>
+        }
+      </div>
+
+      {/* See more button */}
+      <button
+      onClick={ () => setExpand(!expand) }
+      className='w-fit mx-auto mt-6 text-lg md:text-xl font-extrabold tracking-wide cursor-pointer
+      px-4 py-2 rounded flex items-center justify-center gap-2 border border-secondary'>
+        { expand ? 'Read Less' : 'Read More' }
+        <FontAwesomeIcon icon={ expand ? faArrowUp : faArrowDown } 
+        className='transition-all duration-500' />
+      </button>
     </section>
   )
 }
