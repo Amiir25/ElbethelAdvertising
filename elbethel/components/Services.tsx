eec2b7cd24334f1544d0aa410f3afa2a622893b5
Amiir25 <amirsadik636@gmail.com>
2025-10-30 06:58:22 +0300

    Replace <Image> by <img> in Services.tsx

--- elbethel/components/Services.tsx
+++ elbethel/components/Services.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import { services } from '@/constants/services'
 import Image from 'next/image'
-import React from 'react'
 import Title from './Title'
 
+
 const Services = () => {
+
   return (
     <section className='mt-40'>
       
@@ -16,9 +18,11 @@ const Services = () => {
       <div className='flex flex-col md:flex-row flex-wrap items-center justify-center gap-10'>
         {
           services.map((service, i) => (
-            <div key={i} className='mt-10 w-80 md:w-120'>
-              <Image src={service.image} alt={service.title} className='rounded-xl' />
-              <div className='mt-2'>
+            <div key={i} className='mt-10'>
+              <div className='w-80 md:w-120 h-80'>
+                <img src={ service.image.src } alt={service.title } className='rounded-xl object-cover w-full h-full' />
+              </div>
+              <div className='mt-2 max-w-120'>
                 <h1 className='text-xl font-bold'>{ service.title }</h1>
                 <p className='text-sm'>{ service.description }</p>
               </div>
