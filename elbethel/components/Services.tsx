d37eecd73a8a35b08b6125fda64b73270bef758c
Amiir25 <amirsadik636@gmail.com>
2025-10-30 07:48:07 +0300

    Update the horizontal position of items in Services.tsx

--- elbethel/components/Services.tsx
+++ elbethel/components/Services.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { services } from '@/constants/services'
-import Image from 'next/image'
 import Title from './Title'
 
 
@@ -18,11 +16,11 @@ const Services = () => {
       <div className='flex flex-col md:flex-row flex-wrap items-center justify-center gap-10'>
         {
           services.map((service, i) => (
-            <div key={i} className='mt-10'>
-              <div className='w-80 md:w-120 h-80'>
+            <div key={i} className='mt-10 flex flex-col items-center'>
+              <div className='w-80 md:w-120 h-80 md:h-120'>
                 <img src={ service.image.src } alt={service.title } className='rounded-xl object-cover w-full h-full' />
               </div>
-              <div className='mt-2 max-w-120'>
+              <div className='mt-2 max-w-80'>
                 <h1 className='text-xl font-bold'>{ service.title }</h1>
                 <p className='text-sm'>{ service.description }</p>
               </div>
