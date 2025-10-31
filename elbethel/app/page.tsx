f5da1c756a59ee9adea493c0917180b4607decb9
Amiir25 <amirsadik636@gmail.com>
2025-10-31 19:06:46 +0300

    Update page.tsx

--- elbethel/app/page.tsx
+++ elbethel/app/page.tsx
@@ -1,11 +1,11 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
-// import Portfolio from "@/components/RecentProjects";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
-// import Testimonials2 from "@/components/Testimonials2";
 import WhyChoose from "@/components/WhyChoose";
+import Products from "@/components/Products";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
@@ -13,9 +13,9 @@ export default function Home() {
       <Hero/>
       <About/>
       <Services/>
-      {/* <Testimonials2/> */}
-      {/* <Portfolio/> */}
+      <Portfolio/>
       <WhyChoose/>
+      <Products/>
       <Testimonials/>
       <Contact/>
     </>
