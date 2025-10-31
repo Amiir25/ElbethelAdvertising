3d370fb8a4a1e67c353401f2411316ac3739b54b
Amiir25 <amirsadik636@gmail.com>
2025-10-31 19:22:04 +0300

    Add portfolio.ts

---
+++ elbethel/constants/portfolio.ts
@@ -0,0 +1,31 @@
+import abyssinia from '../public/portfolio/abissinia-bank.jpg';
+import addisAbaba from '../public/portfolio/addis-ababa.jpg';
+import awashInsurance from '../public/portfolio/awash-insurance.jpg';
+import bimahiRealestate from '../public/portfolio/bimahi-realestate.jpg';
+import church from '../public/portfolio/church.jpg';
+import dellaMobile from '../public/portfolio/della-mobile.jpg';
+import eshetuMamo from '../public/portfolio/eshetu-mamo.jpg';
+import friendship from '../public/portfolio/friendship.jpg';
+import madoHotel from '../public/portfolio/mado-hotel.jpg';
+import magnusCar from '../public/portfolio/magnus-car.jpg';
+import mamokacha from '../public/portfolio/mamokacha.jpg';
+import noahRealestate from '../public/portfolio/noah-realestate.jpg';
+import sunshine from '../public/portfolio/sunshine.jpg';
+import zimbabweEmbassy from '../public/portfolio/zimbabwe-embassy.jpg';
+
+export const ourPortfolio = [
+    { id: 1, image: abyssinia },
+    { id: 2, image: addisAbaba },
+    { id: 3, image: awashInsurance },
+    { id: 4, image: bimahiRealestate },
+    { id: 5, image: church },
+    { id: 6, image: dellaMobile },
+    { id: 7, image: eshetuMamo },
+    { id: 8, image: friendship },
+    { id: 10, image: madoHotel },
+    { id: 11, image: magnusCar },
+    { id: 12, image: mamokacha },
+    { id: 13, image: noahRealestate },
+    { id: 14, image: sunshine },
+    { id: 15, image: zimbabweEmbassy },
+];
