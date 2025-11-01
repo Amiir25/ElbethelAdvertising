59b5b94fbdeb0207bc7a405a2322131718a0702f
Amiir25 <amirsadik636@gmail.com>
2025-11-01 21:51:49 +0300

    Update components

--- elbethel/constants/navLinks.ts
+++
@@ -1,7 +0,0 @@
-export const navLinks = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/about', key: 'about', label: 'About' },
-    { href: '/services', key: 'services', label: 'Services' },
-    { href: '/portfolio', key: 'portfolio', label: 'Portfolio' },
-    { href: '/contact', key: 'contact', label: 'Contact' },
-];
