b5c7fe7abd063fb4e3889d3f91d1b91fef08fc52
Amiir25 <amirsadik636@gmail.com>
2025-11-01 22:20:22 +0300

    Update assets in assets directory

---
+++ elbethel/assets/contact.ts
@@ -0,0 +1,21 @@
+import { faMailForward } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons/faLocationDot";
+import { faPhone } from "@fortawesome/free-solid-svg-icons/faPhone";
+
+export const contacts = [
+    {
+        icon: faLocationDot,
+        title: 'Address',
+        description: 'Bole Adey Ababa, Addis Abeba',
+    },
+    {
+        icon: faPhone,
+        title: 'Phone',
+        description: '+251 900 000 000',
+    },
+    {
+        icon: faMailForward,
+        title: 'Email',
+        description: 'info@elbethel.com',
+    },
+];
---
+++ elbethel/assets/services.ts
@@ -0,0 +1,51 @@
+import banner from '../public/service-images/banner.webp';
+import indoorAdvert from '../public/service-images/indoor-advret.webp';
+import interiorDesign from '../public/service-images/interior-design.webp';
+import lightBox from '../public/service-images/light-box.webp';
+import TShirt from '../public/service-images/T-shirt.webp';
+import vehicleAdvert from '../public/service-images/vehicle-advert.webp';
+
+export const services = [
+    {
+        id: 1,
+        image: lightBox,
+        title: 'Light Boxes & LED Signage',
+        description: 'Design and installation of light0boxes, LED lighting letters and neon-style displays that bring your brand to life.',
+    },
+    {
+        id: 2,
+        image: indoorAdvert,
+        title: 'Indoor & Outdoor Advertising',
+        description: 'Professional branding for walls and storefronts — built to keep your message clear.',
+    },
+    {
+        id: 3,
+        image: banner,
+        title: 'Large Format Digital Printing',
+        description: 'High-resolution banners, posters, and wall graphics using premium materials and vibrant inks.',
+    },
+    {
+        id: 4,
+        image: vehicleAdvert,
+        title: 'Vehicle Branding',
+        description: 'Transform company cars into mobile advertisements with long-lasting vehicle wraps and decals.',
+    },
+    {
+        id: 5,
+        image: interiorDesign,
+        title: 'Interior Signs & Wall Branding',
+        description: 'Create stunning branded environments with interior wall graphics and decorative installations.',
+    },
+    {
+        id: 6,
+        image: TShirt,
+        title: 'Promotional Printing',
+        description: 'Custom printing on T-shirts, pens, medals, mugs, and key holders for marketing campaigns or events.',
+    },
+    // {
+    //     id: 7,
+    //     image: CNC,
+    //     title: 'Printing Materials Supply',
+    //     description: 'Reliable supply of printing and advertising materials for signage professionals.',
+    // },
+];
