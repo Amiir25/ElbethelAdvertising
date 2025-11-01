12c5e9f8cb9582946a209faf486f5b43f1e7770a
Amiir25 <amirsadik636@gmail.com>
2025-11-01 22:26:40 +0300

    Add assets for about page in about/assets

---
+++ elbethel/app/about/assets/assets.ts
@@ -0,0 +1,54 @@
+// Images
+import bgImage from './bg-about.webp'
+import image1 from './about-image-1.webp'
+import image2 from './about-image-2.webp'
+import image3 from './about-image-3.webp'
+
+export const images = {
+    bgImage, image1, image2, image3
+};
+
+// Mission, Vision, and Values
+export const missions = [
+    {
+        title: 'Mission',
+        description: `To provide dependable, creative, and high-quality signage and printing solutions that help
+        local businesses communicate their identity clearly and attractively.`,
+    },
+    {
+        title: 'Vision',
+        description: `To be a go-to signage and printing partner in Ethiopia — known for quality, innovation,
+        and equipment supply that empowers others in the industry.`,
+    },
+    {
+        title: 'Values',
+        description: `Creativity • Quality • Precision • Reliability • Customer focus`,
+    },
+];
+
+// Clients
+import abyssiniaBankLogo from './abyssinia-logo.webp';
+import boraLogo from './bora-logo.webp';
+import burgerizaLogo from './burgerizza-logo.webp';
+import castleLogo from './castle-logo.webp';
+import chickenHutLogo from './chicken-hut-logo.webp';
+import madoHotelLogo from './mado-hotel-logo.webp';
+import kadiscoLogo from './kadisco-logo.webp';
+import mamokachaLogo from './mamokacha.webp';
+import noahLogo from './noah-logo.webp';
+import siketBankLogo from './siket-bank-logo.webp';
+import sunriseLogo from './sunrise-logo.webp';
+
+export const clients = [
+    { image: abyssiniaBankLogo, name: 'Bank Of Abyssinia' },
+    { image: boraLogo, name: 'Bora Amusement park' },
+    { image: burgerizaLogo, name: 'Burgerizza' },
+    { image: castleLogo, name: 'Castle Familial & International' },
+    { image: chickenHutLogo, name: 'Chicken Hut' },
+    { image: madoHotelLogo, name: 'Mado Hotel' },
+    { image: kadiscoLogo, name: 'Kadisco Asian Paints' },
+    { image: mamokachaLogo, name: 'Mamokacha' },
+    { image: noahLogo, name: 'Noah Realestate' },
+    { image: siketBankLogo, name: 'Siket bank' },
+    { image: sunriseLogo, name: 'Sunrise' },
+];
