7d079db3d22a2ebada1337171d453911dd1c43d7
Amiir25 <amirsadik636@gmail.com>
2025-11-04 19:56:15 +0300

    Rename Portfolio.tsx to OurWork.tsx

--- elbethel/components/OurWork.tsx
+++ elbethel/components/OurWork.tsx
@@ -2,38 +2,38 @@
 
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { ourPortfolio } from "@/assets/portfolio";
+import { ourWork } from "@/assets/portfolio";
 import Title from "./Title";
 import Link from "next/link";
 import CTAButton from "./CTAButton";
 
 
-const Portfolio = () => {
+const OurWork = () => {
 
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setIndex((prev) => (prev + 1) % ourPortfolio.length);
+      setIndex((prev) => (prev + 1) % ourWork.length);
     }, 3000);
 
     return () => clearInterval(timer);
   }, []);
 
-  const portfolio = ourPortfolio[index];
+  const work = ourWork[index];
 
   return (
     <section className="px-6 md:px-12 lg:px-24 xl:px-32 mt-40 overflow-hidden">
 
       <Title 
         normalTitle="Our"
-        underlineTitle="Portfolio"
+        underlineTitle="work"
         subtitle="See how we help brands light up their identity" />
 
       <div className="relative w-full h-[60vh] md:h-[70vh] mt-10">
         <AnimatePresence mode="sync">
           <motion.div
-            key={portfolio.id}
+            key={work.id}
             initial={{ opacity: 0, x: 1000 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -1000 }}
@@ -41,7 +41,7 @@ const Portfolio = () => {
             className="absolute inset-0 w-full">
             
             <div className="w-full h-full overflow-hidden rounded-xl">
-              <img src={ portfolio.image.src } alt=""
+              <img src={ work.image.src } alt=""
               className="w-full h-full object-cover" />
             </div>
 
@@ -52,7 +52,7 @@ const Portfolio = () => {
       {/* Navigation indicator */}
       <div className="flex items-center justify-center space-x-2 mt-6">
         {
-          ourPortfolio.map((p, i) => (
+          ourWork.map((p, i) => (
             <button
               key={p.id}
               onClick={() => setIndex(i)}
@@ -64,11 +64,11 @@ const Portfolio = () => {
       </div>
 
       {/* CTA */}
-      <Link href={'/'} >
+      <Link href={'/projects'} >
         <CTAButton cta='More Projects' />
       </Link>
     </section>
   );
 }
 
-export default Portfolio;
+export default OurWork;
