c131d7628e780d57f2301f392f7803ec24b34832
Amiir25 <amirsadik636@gmail.com>
2025-11-04 11:51:44 +0300

    Add asset files for projects page in projects/assets/

---
+++ elbethel/app/projects/assets/assets.ts
@@ -0,0 +1,103 @@
+import addisHotel from './addis-hotel.webp';
+import adottinaHotel from './adottina-hotel.webp';
+import akermaPension from './akerma-pension.webp';
+import almiDental from './almi-dental.webp';
+import amanMotor from './aman-motor.webp';
+import amenDecore from './amen-decore.webp';
+import amirChineseRestaurant from './amir-chinese-restaurant.webp';
+import barberShop from './barber-shop.webp';
+import barocHotel from './baroc-hotel.webp';
+import blackBarber from './black-barber.webp';
+import btimesCar from './btimes-car.webp';
+import bubbles from './bubbles.webp';
+import centerAddis from './center-addis.webp';
+import coffeeFirst from './coffee-first.webp';
+import dagiFashion from './dagi-fashion.webp';
+import daniGranite from './dani-granite.webp';
+import davyAluminum from './davy-aluminum.webp';
+import dealTravel from './deal-travel.webp';
+import dentcareDental from './dentcare-dental.webp';
+import desireCar from './desire-car.webp';
+import doloCoffee from './dolo-coffee.webp';
+import dorcas from './dorcas.webp';
+import dreamMassage from './dream-massage.webp';
+import elroeBeauty from './elroe-beauty.webp';
+import entotoChurch from './entoto-church.webp';
+import etenuPharmacy from './etenu-pharmacy.webp';
+import ethioClinik from './ethio-clinik.webp';
+import fassiloWood from './fassilo-wood.webp';
+import gebioch from './gebioch.webp';
+import gelilaPharmacy from './gelila-pharmacy.webp';
+import grandBakery from './grand-bakery.webp';
+import heavenBurger from './heaven-burger.webp';
+import henockCar from './henock-car.webp';
+import jebaManufacturing from './jeba-manufacturing.webp';
+import jojoCar from './jojo-car.webp';
+import kadisco from './kadisco.webp';
+import kannFerniture from './kann-ferniture.webp';
+import langanoFish from './langano-fish.webp';
+import leChateau from './le-chateau.webp';
+import ljChurch from './lj-church.webp';
+import lounge from './lounge.webp';
+import lucidDental from './lucid-dental.webp';
+import luxuryCarWash from './luxury-car-wash.webp';
+import mafiCitymall from './mafi-citymall.webp';
+import magnusCar from './magnus-car.webp';
+import margoCoffee from './margo-coffee.webp';
+import mehalFuri from './mehal-furi.webp';
+import modelPhoto from './model-photo.webp';
+import muko from './muko.webp';
+import newVision from './new-vision.webp';
+import nobleCafe from './noble-cafe.webp';
+import payway from './payway.webp';
+import pclClinik from './pcl-clinik.webp';
+import qawaArada from './qawa-arada.webp';
+import redBeauty from './red-beauty.webp';
+import redfox from './redfox.webp';
+import rungo from './rungo.webp';
+import salemKids from './salem-kids.webp';
+import secondChanceMassage from './second-chance-massage.webp';
+import secretMassage from './secret-massage.webp';
+import sewerSifet from './sewer-sifet.webp';
+import shay from './shay.webp';
+import shuShiyangRestaurant from './shu-shiyang-restaurant.webp';
+import siketBank from './siket-bank.webp';
+import sony from './sony.webp';
+import stgabrielHospital from './stgabriel-hospital.webp';
+import tafTrading from './taf-trading.webp';
+import tesheab from './tesheab.webp';
+import theshotBar from './theshot-bar.webp';
+import timoniorGarment from './timonior-garment.webp';
+import tomatelli from './tomatelli.webp';
+import tsiBonda from './tsi-bonda.webp';
+import velvet from './velvet.webp';
+import veronicaBarber from './veronica-barber.webp';
+import viva from './viva.webp';
+import xoLiquor from './xo-liquor.webp';
+import yafiClassic from './yafi-classic.webp';
+import yalGold from './yal-gold.webp';
+import ydBingo from './yd-bingo.webp';
+import yeHagere from './ye-hagere.webp';
+import yegnaDesign from './yegna-design.webp';
+import yemamEndris from './yemam-endris.webp';
+import yoliHotel from './yoli-hotel.webp';
+
+export const images = [
+    addisHotel, adottinaHotel, akermaPension, almiDental, amanMotor,
+    amenDecore, amirChineseRestaurant, barberShop, barocHotel, blackBarber,
+    btimesCar, bubbles, centerAddis, coffeeFirst, dagiFashion,
+    daniGranite, davyAluminum, dealTravel, dentcareDental, desireCar,
+    doloCoffee, dorcas, dreamMassage, elroeBeauty, entotoChurch,
+    etenuPharmacy, ethioClinik, fassiloWood, gebioch, gelilaPharmacy,
+    grandBakery, heavenBurger, henockCar, jebaManufacturing, jojoCar,
+    kadisco, kannFerniture, langanoFish, leChateau, ljChurch,
+    lounge, lucidDental, luxuryCarWash, mafiCitymall, magnusCar,
+    margoCoffee, mehalFuri, modelPhoto, muko, newVision,
+    nobleCafe, payway, pclClinik, qawaArada, redBeauty,
+    redfox, rungo, salemKids, secondChanceMassage, secretMassage,
+    sewerSifet, shay, shuShiyangRestaurant, siketBank, sony,
+    stgabrielHospital, tafTrading, tesheab, theshotBar, timoniorGarment,
+    tomatelli, tsiBonda, velvet, veronicaBarber, viva,
+    xoLiquor, yafiClassic, yalGold, ydBingo, yeHagere,
+    yegnaDesign, yemamEndris, yoliHotel,
+];
