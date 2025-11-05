0ade8556595dd53e88d3bed34c042962a9090183
Amiir25 <amirsadik636@gmail.com>
2025-11-05 12:13:08 +0300

    dicrease the gap between titles and subtitles in Title.tsx

--- elbethel/components/Title.tsx
+++ elbethel/components/Title.tsx
@@ -10,7 +10,7 @@ const Title = ({ normalTitle, underlineTitle, subtitle }: title) => {
         <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold text-center'>
           { normalTitle } <span className='underline underline-offset-6 decoration-5 decoration-[#ff4000]'>{ underlineTitle }</span>
         </h1>
-        <p className='text-center text-sm md:text-base font-light mt-4'>
+        <p className='text-center text-sm md:text-base font-light mt-2'>
           { subtitle }
         </p>
 
