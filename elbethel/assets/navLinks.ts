284b6c8f74a5f83882e779663593686d9c761f64
Amiir25 <amirsadik636@gmail.com>
2025-11-05 15:16:42 +0300

    Remove Shops link from navbar in assets/navLinks.ts

--- elbethel/assets/navLinks.ts
+++ elbethel/assets/navLinks.ts
@@ -3,6 +3,6 @@ export const navLinks = [
     { href: '/about', key: 'about', label: 'About' },
     { href: '/services', key: 'services', label: 'Services' },
     { href: '/projects', key: 'projects', label: 'Our Work' },
-    { href: '/products', key: 'products', label: 'Shop' },
+    // { href: '/products', key: 'products', label: 'Shop' },
     { href: '/contact', key: 'contact', label: 'Contact' },
 ];
