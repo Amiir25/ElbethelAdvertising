562fed83dc6721588248bd48d95ff9ae1c13d366
Amiir25 <amirsadik636@gmail.com>
2025-11-05 11:46:00 +0300

    Rename assets/portfolio.ts to assets/ourWork.ts

---
+++ elbethel/assets/ourWork.ts
@@ -0,0 +1,39 @@
+import abyssinia from '../public/our-work/abissinia-bank.webp';
+import addisAbaba from '../public/our-work/addis-ababa.webp';
+import awashInsurance from '../public/our-work/awash-insurance.webp';
+import bimahiRealestate from '../public/our-work/bimahi-realestate.webp';
+import church from '../public/our-work/church.webp';
+import dellaMobile from '../public/our-work/della-mobile.webp';
+import eshetuMamo from '../public/our-work/eshetu-mamo.webp';
+import friendship from '../public/our-work/friendship.webp';
+import madoHotel from '../public/our-work/mado-hotel.webp';
+import magnusCar from '../public/our-work/magnus-car.webp';
+import mamokacha from '../public/our-work/mamokacha.webp';
+import noahRealestate from '../public/our-work/noah-realestate.webp';
+import sunshine from '../public/our-work/sunshine.webp';
+import zimbabweEmbassy from '../public/our-work/zimbabwe-embassy.webp';
+import caramelPastry from '../public/our-work/caramel-pastry.webp';
+import carmiSchool from '../public/our-work/carmi-school.webp';
+import etOnline from '../public/our-work/et-online.webp';
+import tesheabBusinessCenter from '../public/our-work/tesheab-business-center.webp';
+
+export const ourWork = [
+    { id: 1, image: abyssinia },
+    { id: 2, image: addisAbaba },
+    { id: 3, image: awashInsurance },
+    { id: 4, image: bimahiRealestate },
+    { id: 5, image: church },
+    { id: 6, image: dellaMobile },
+    { id: 7, image: eshetuMamo },
+    { id: 8, image: friendship },
+    { id: 10, image: madoHotel },
+    { id: 11, image: magnusCar },
+    { id: 12, image: mamokacha },
+    { id: 13, image: noahRealestate },
+    { id: 14, image: sunshine },
+    { id: 15, image: zimbabweEmbassy },
+    { id: 16, image: caramelPastry },
+    { id: 17, image: carmiSchool },
+    { id: 18, image: etOnline },
+    { id: 19, image: tesheabBusinessCenter },
+];
