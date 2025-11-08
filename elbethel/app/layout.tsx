19d0a84051f2f2e1311a528de5e9c8ea3cdd99a6
Amiir25 <amirsadik636@gmail.com>
2025-11-08 07:57:45 +0300

    Add favicon images in metadata in layout.tsx

--- elbethel/app/layout.tsx
+++ elbethel/app/layout.tsx
@@ -8,8 +8,18 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 export const metadata: Metadata = {
-  title: "Elbethel",
+  title: "Elbethel Advertising",
   description: "Elbethel Advertising",
+  icons: {
+    icon: [
+      { url: '/favicons/favicon.ico' },
+      { url: '/favicons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/favicons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: { url: '/favicons/apple-touch-icon.png' },
+  }
 };
 
 export default function RootLayout({
